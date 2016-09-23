d2e34c43ab6b84fc3c9c5bd858b854f643090fd2
Claudia Stamfel <claudia@y7mail.com>
2016-09-23 16:26:02 +1000

    introduced parsley. You lose points if you kill parsley

--- cilantro.js
+++ cilantro.js
@@ -4,12 +4,15 @@ var viewportHeight = Number(window.innerHeight,10),
 	setCilantro = function() {
         var randX = Math.random() * viewportWidth,
             randY = Math.random() * viewportHeight,
-            cilDiv = document.createElement('div');
-
-        cilDiv.className = 'cilantro';
+            cilDiv = document.createElement('div'),
+			totScore = Number(document.getElementById('scoreNum').innerHTML,10),
+			div3 = Number(document.getElementById('scoreNum').innerHTML,10) % 3;
+			
+		cilDiv.className = 'cilantro';
         document.getElementsByTagName('body')[0].appendChild(cilDiv);
         cilDiv.style.left = randX + 'px';
         cilDiv.style.top = randY + 'px';
+        setTimeout(function(){cilDiv.style.display = "none"}, 3000);
         cilDiv.onclick = function(e){
         		var scoreDiv = document.getElementById('scoreNum');
         		this.className += " on";
@@ -18,6 +21,24 @@ var viewportHeight = Number(window.innerHeight,10),
 	        	score = score + 1;
 	        	scoreDiv.innerHTML = score;		        
 	    };
+	   if (div3 === 0  && totScore != 0) {
+	   		var parDiv = document.createElement('div');
+		   	parDiv.className = 'parsley';
+		   	console.log(parDiv);
+		    document.getElementsByTagName('body')[0].appendChild(parDiv);
+			parDiv.style.left = randX + 'px';
+			parDiv.style.top = randY + 'px';
+			parDiv.onclick = function(e){
+        		var scoreDiv = document.getElementById('scoreNum');
+        		this.className += " on";
+	        	setTimeout(function(){parDiv.style.display = "none";}, 500);
+	        	score = Number(scoreDiv.innerHTML,10);
+	        	score = score -10;
+	        	scoreDiv.innerHTML = score;		        
+	    };
+		   
+	   }
+	   
 
     };
 document.getElementsByTagName('body')[0].appendChild(scoreSign),
